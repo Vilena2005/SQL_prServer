f02d3c88df0d0c292b5a5b2b777be978af4738af
Activate <164019370+Vilena2005@users.noreply.github.com>
2025-11-26 14:34:44 +0700

    Add files via upload

---
+++ mvc.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Ноя 26 2025 г., 08:19
+-- Версия сервера: 10.4.32-MariaDB
+-- Версия PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `abonents`
+--
+
+CREATE TABLE `abonents` (
+  `id` int(11) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `patronym` varchar(100) NOT NULL,
+  `birth_date` date NOT NULL,
+  `division_id` int(11) DEFAULT NULL,
+  `phone` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `abonents`
+--
+
+INSERT INTO `abonents` (`id`, `surname`, `name`, `patronym`, `birth_date`, `division_id`, `phone`) VALUES
+(13, 'Сергеев', 'Сергей', 'Сергеевич', '2000-01-01', 21, 2147483647);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `divisions`
+--
+
+CREATE TABLE `divisions` (
+  `id` int(11) NOT NULL,
+  `division_name` varchar(100) NOT NULL,
+  `division_type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `divisions`
+--
+
+INSERT INTO `divisions` (`id`, `division_name`, `division_type`) VALUES
+(12, 'Свободу бобрам', 'Отдел по борьбе за свободу'),
+(14, 'Центр тестирования', 'Отдел разработки'),
+(21, 'Сборочный цех', 'Производственный отдел'),
+(26, 'Подразделение', 'Вид подразделения'),
+(27, 'Название подразделения', 'Вид подразделения'),
+(28, 'Сборочный цех', 'Отдел разработки');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `text`) VALUES
+(1, 'Философия', 'Форма рационально-теоретического мышления, имеющая своим предметом исследование предельных характеристик бытия, взаимоотношения человека и мира.'),
+(2, 'Программирование', 'Это процесс создания компьютерных программ, которые выполняют определённые задачи. В основе программирования лежит написание кода на специальных языках программирования.');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `room_number` int(11) NOT NULL,
+  `room_type` varchar(255) NOT NULL,
+  `division_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `login`, `password`, `role`) VALUES
+(2, 'User1', 'UserLogin', 'd1a5ff8dbeedaa3406368724ebbd3cb0', 1),
+(14, 'Алексей', 'AlexLogin', '8068c76c7376bc08e2836ab26359d4a4', 1),
+(16, 'Admin', 'AdminLogin', '8068c76c7376bc08e2836ab26359d4a4', 3),
+(21, 'Алексей', 'afaf', 'aaad1fe5ee4c7125dda360ff6c8209a2', 1),
+(23, 'User', 'Login', '9c6aebdb1ff352eda04bd28bf4e4cc61', 1);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `abonents`
+--
+ALTER TABLE `abonents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `division_id` (`division_id`);
+
+--
+-- Индексы таблицы `divisions`
+--
+ALTER TABLE `divisions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `division_id` (`division_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `abonents`
+--
+ALTER TABLE `abonents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT для таблицы `divisions`
+--
+ALTER TABLE `divisions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT для таблицы `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `abonents`
+--
+ALTER TABLE `abonents`
+  ADD CONSTRAINT `abonents_ibfk_1` FOREIGN KEY (`division_id`) REFERENCES `divisions` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`division_id`) REFERENCES `divisions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
